dd2b17b62060c34d0580bd90d8b19a24d096a8de
maytonka <stu-pix_dazalone@hotmail.com>
2014-05-01 17:25:15 +0700

    project android
    55555

--- AEDE/src/com/example/aede/OnResponseListener.java
+++
@@ -1,9 +0,0 @@
-package com.example.aede;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-
-public interface OnResponseListener
-{
-	void onResponse(ArrayList<HashMap<String, String>> list);
-}
